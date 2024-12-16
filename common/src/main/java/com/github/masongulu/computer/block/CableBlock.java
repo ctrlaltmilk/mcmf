367a3b931bb62ff4040e8c9c862ddbe9eb537c76
ctrlaltmilk <iris@ctrlaltmilk.net>
2024-12-15 19:06:29 -0800

    Rework ribbon cable models/textures

--- common/src/main/java/com/github/masongulu/computer/block/CableBlock.java
+++ common/src/main/java/com/github/masongulu/computer/block/CableBlock.java
@@ -2,28 +2,40 @@ package com.github.masongulu.computer.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.RenderShape;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.RedstoneSide;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class CableBlock extends Block {
-    public static final VoxelShape BOTTOM_SLAB = Block.box(0.0, 0.0, 0.0, 16.0, 8.0, 16.0);
+    public static final EnumProperty<RedstoneSide> NORTH = BlockStateProperties.NORTH_REDSTONE;
+    public static final EnumProperty<RedstoneSide> EAST = BlockStateProperties.EAST_REDSTONE;
+    public static final EnumProperty<RedstoneSide> SOUTH = BlockStateProperties.SOUTH_REDSTONE;
+    public static final EnumProperty<RedstoneSide> WEST = BlockStateProperties.WEST_REDSTONE;
+
+    public static final VoxelShape SHAPE = Block.box(0.0, 0.0, 0.0, 16.0, 1.0, 16.0);
+
     public CableBlock() {
-        super(Properties.of(Material.STONE));
+        super(Properties.of(Material.DECORATION).noCollission().instabreak());
+        registerDefaultState(getStateDefinition().any()
+                .setValue(NORTH, RedstoneSide.NONE)
+                .setValue(EAST, RedstoneSide.NONE)
+                .setValue(SOUTH, RedstoneSide.NONE)
+                .setValue(WEST, RedstoneSide.NONE));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(NORTH, EAST, SOUTH, WEST);
     }
 
     @Override
     public VoxelShape getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
-        return BOTTOM_SLAB;
-    }
-
-    @Override
-    public void onRemove(BlockState blockState, Level level, BlockPos blockPos, BlockState blockState2, boolean bl) {
-        super.onRemove(blockState, level, blockPos, blockState2, bl);
+        return SHAPE;
     }
 }
--- common/src/main/java/com/github/masongulu/serial/block/SerialCableBlock.java
+++ common/src/main/java/com/github/masongulu/serial/block/SerialCableBlock.java
@@ -1,27 +1,11 @@
 package com.github.masongulu.serial.block;
 
+import com.github.masongulu.computer.block.CableBlock;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.Material;
-import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.VoxelShape;
 
-import static com.github.masongulu.computer.block.CableBlock.BOTTOM_SLAB;
-
-public class SerialCableBlock extends Block {
-    public SerialCableBlock() {
-        super(Properties.of(Material.STONE));
-    }
-
-    @Override
-    public VoxelShape getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
-        return BOTTOM_SLAB;
-    }
-
+public class SerialCableBlock extends CableBlock {
     @Override
     public void onRemove(BlockState blockState, Level level, BlockPos blockPos, BlockState blockState2, boolean bl) {
         super.onRemove(blockState, level, blockPos, blockState2, bl);
